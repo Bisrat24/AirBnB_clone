8e01faab9a36dcb607be25c729e90810fbc69b94
Bisrat24 <bisrattam@gmail.com>
2022-09-04 14:37:35 +0300

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,43 +1,77 @@
 #!/usr/bin/python3
-"""test BaseModel"""
+"""
+Module for unittests for the BaseModel class
+"""
 import unittest
-import os
+import datetime
 from models.base_model import BaseModel
-import pep8
 
-class TestBaseModel(unittest.TestCase):
-    """test BaseModel"""
+
+class TestBaseModelClassCreation(unittest.TestCase):
+    """Test class for Base class instantiation tests"""
 
     def setUp(self):
-        self.testbasemodel = BaseModel()
+        self.x = BaseModel()
 
-    def test_pep8_BaseModel(self):
-        """Testing for pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/base_model.py'])
-        self.assertEqual(p.total_errors, 0, "Check pep8")
+    def test_id_creation(self):
+        self.assertIsNotNone(self.x.id)
+        self.assertEqual(36, len(self.x.id))
+        self.assertIsInstance(self.x.id, str)
+        self.assertFalse(" " in self.x.id)
+
+    def test_created_at(self):
+        self.assertIsNotNone(self.x.created_at)
+        self.assertIsInstance(self.x.created_at, datetime.datetime)
+
+    def test_updated_at(self):
+        self.assertIsNotNone(self.x.updated_at)
+        self.assertIsInstance(self.x.updated_at, datetime.datetime)
+
+    def test_created_at_diff_updated_at(self):
+        self.assertNotEqual(self.x.updated_at, self.x.created_at)
+        time_diff = self.x.updated_at - self.x.created_at
+        self.assertTrue(time_diff.microseconds > 0)
+
+    def test_save_method(self):
+        old_time = self.x.updated_at
+        self.x.save()
+        self.assertNotEqual(old_time, self.x.updated_at)
+        self.assertIsInstance(self.x.updated_at, datetime.datetime)
+
+    def test_to_dict_method(self):
+        dict_ = self.x.to_dict()
+        self.assertIsInstance(dict_, dict)
+        self.assertIsInstance(dict_['updated_at'], str)
+        self.assertIsInstance(dict_['created_at'], str)
+        self.assertEqual(dict_['__class__'],
+                         self.x.__class__.__name__)
 
 
-    def test_save_BaesModel(self):
-        """test save_Basemodel"""
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+class TestBaseModelObjectCreation(unittest.TestCase):
+    """
+    Test class for Base Model instantiation with kwargs
+    """
 
-    def test_doc(self):
-        """ Tests doc """
-        self.assertisNotNone(BaseModel.__doc__)
+    def setUp(self):
+        self.my_model = BaseModel()
+        self.my_model.name = "Betty"
+        self.my_model.my_number = 89
+        self.my_model_json = self.my_model.to_dict()
+        self.my_new_model = BaseModel(**self.my_model_json)
 
-    def test_to_json(self):
-        '''test to jason'''
+    def test_create_object_from_dict(self):
+        self.assertIsInstance(self.my_new_model, BaseModel)
+        self.assertEqual("Betty", self.my_new_model.name)
+        self.assertEqual(89, self.my_new_model.my_number)
+        self.assertIsNotNone(self.my_new_model.created_at)
+        self.assertIsInstance(self.my_new_model.created_at,
+                              datetime.datetime)
+        self.assertIsInstance(self.my_new_model.updated_at,
+                              datetime.datetime)
+        self.assertFalse(self.my_model is self.my_new_model)
 
-    def test_kwarg(self):
-        basemodel = BaseModel(name="base")
-        self.assertEqual(type(basemodel).__name__, "BaseModel")
-        self.assertFalse(hasattr(basemodel, "id"))
-        self.assertFalse(hasattr(basemodel, "created_at"))
-        self.assertTrue(hasattr(basemodel, "name"))
-        self.assertFalse(hasattr(basemodel, "updated_at"))
-        self.assertTrue(hasattr(basemodel, "__class__"))
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_str_method(self):
+        string = "[{}] ({}) {}".format(self.my_model.__class__.__name__,
+                                       self.my_model.id,
+                                       self.my_model.__dict__)
+        self.assertEqual(string, str(self.my_model))
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,101 +1,89 @@
 #!/usr/bin/python3
-""" unit test for class State """
-
-from models.state import State
-import pep8
+"""
+Module for unittests for the State class
+"""
 import unittest
+import os
+import models
+from models.state import State
 
 
-class TestStateDocs(unittest.TestCase):
-    """ Test docstring in the class """
+class TestStateCreation(unittest.TestCase):
+    """Test class for instantiating state"""
 
-    def test_doc_class(self):
-        """ Test document class """
-        doc = State.__doc__
-        assert doc is not None
-
-    def test_doc_methods_class(self):
-        """ Test document methods Class """
-        l_method = ["save", "__init__", "__str__", "to_dict"]
-        for key in State.__dict__.keys():
-            if key is l_method:
-                doc = key.__doc__
-                assert doc is not None
-
-
-class TestState(unittest.TestCase):
-    """ Test creation objects and use methods """
-    @classmethod
-    def setUpClass(cls):
-        ''' new_state up '''
-        cls_state = State()
-        cls_state.name = "Tunis"
-        cls_state.save()
-        cls_state_str = cls_state.to_dict()
-
-    def test_create_object(self):
-        """ Test created instance """
-        self.assertIsInstance(self.new_state, State)
-
-    def test_string_representation(self):
-        """ Test string representation """
-        rep_str = str(self.new_state)
-        list = ['State', 'id', 'created_at', 'updated_at']
-        num = 0
-        for att in list:
-            if att in rep_str:
-                num += 216
-        self.assertTrue(4 == num)
-
-    def test_method_save(self):
-        """ Test save method """
-        current = self.new_state.updated_at
-        self.new_state.save()
-        new = self.new_state.updated_at
-        self.assertNotEqual(current, new)
-
-    def test_hasMethods(self):
-        ''' test the instance have the methods  '''
-        self.assertTrue(hasattr(self.new_state, '__str__'))
-        self.assertTrue(hasattr(self.new_state, '__init__'))
-        self.assertTrue(hasattr(self.new_state, 'to_dict'))
-        self.assertTrue(hasattr(self.new_state, 'save'))
-
-    def test_add_attributes(self):
-        """ add attributes to object"""
-        self.new_state.name = "Tunis"
-        list = [self.new_state.name]
-        expected = ["Tunis"]
-        self.assertEqual(expected, list)
-
-    def test_method_to_dict(self):
-        self.new_state.name = "Tunis"
-        dict_rep = self.new_state.to_dict()
-        list = ['id', 'created_at', 'updated_at',
-                    'name', '__class__']
-        num = 0
-        for att in dict_rep.keys():
-            if att in list:
-                num += 216
-        self.assertTrue(5 == num)
-
-    def test_pep8_conformance(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files([
-                                        'models/state.py',
-                                        'tests/test_models/test_state.py'
-                                        ])
-        self.assertEqual(p.total_errors, 0, "Check pep8")
-
-    @classmethod
-    def tearDownClass(cls):
-        ''' Test new state Down '''
-        del cls_state
+    def setUp(self):
+        self.file = 'file.json'
         try:
-            os.remove("objects.json")
-        except BaseException:
+            os.remove(self.file)
+        except:
+            pass
+        self.x = State()
+        self.validAttributes = {
+            'name': str,
+            }
+        self.storage = models.storage
+
+    def tearDown(self):
+        try:
+            os.remove(self.file)
+        except:
             pass
 
-if __name__ == '__main__':
-    unittest.main()
+    def createState(self):
+        self.ex = State()
+        self.ex.name = "New York"
+
+    def test_has_correct_class_name(self):
+        self.assertEqual('State', self.x.__class__.__name__)
+
+    def test_empty_has_attrs(self):
+        for k in self.validAttributes:
+            self.assertTrue(hasattr(self.x, k))
+
+    def test_empty_attrs_type(self):
+        for k, v in self.validAttributes.items():
+            test_type = type(getattr(self.x, k))
+            self.assertEqual(test_type, v)
+
+    def test_added_attrs(self):
+        self.createState()
+        self.assertEqual(self.ex.name, "New York")
+
+    def test_check_custom_attrs(self):
+        self.x.custom_attr = "Test"
+        self.assertEqual(self.x.custom_attr, "Test")
+        self.assertIsInstance(self.x.custom_attr, str)
+
+    def test_save_time_change(self):
+        old_time = self.x.updated_at
+        self.x.save()
+        self.assertNotEqual(self.x.updated_at, old_time)
+
+    def test_new_dict(self):
+        self.createState()
+        dict_ = self.ex.to_dict()
+        self.y = State(**dict_)
+        self.assertEqual(self.ex.name, self.y.name)
+
+    def test_new_dict_attr_types(self):
+        self.createState()
+        dict_ = self.ex.to_dict()
+        self.y = State(**dict_)
+        for k, v in self.validAttributes.items():
+            test_type = type(getattr(self.y, k))
+            self.assertEqual(test_type, v)
+
+    def test_save(self):
+        self.assertIsInstance(self.storage._FileStorage__objects, dict)
+        self.storage.save()
+        self.assertTrue(os.path.exists(self.file))
+        self.assertTrue(os.stat(self.file).st_size != 0)
+
+    def test_reload(self):
+        x_id = self.x.id
+        x_id_key = "{}.{}".format(self.x.__class__.__name__, self.x.id)
+        self.storage.save()
+        self.storage._FileStorage__objects = {}
+        self.storage.reload()
+        self.assertEqual(x_id,
+                         self.storage._FileStorage__objects[x_id_key].id)
